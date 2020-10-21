d35d458b8521f9a40000f52638a35494c39b0e21
Caitlin Ashcroft <caitlin.ashcroft@keble.ox.ac.uk>
2020-10-21 16:40:36 +0100

    #17 Add blank lines to fix flake8 erros

--- prototype.py
+++ prototype.py
@@ -2,17 +2,23 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.integrate
 
+
 def dose(t, X):
+
     return X
 
+
 def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+
     q_c, q_p1 = y
     transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
     dqc_dt = dose(t, X) - q_c / V_c * CL - transition
     dqp1_dt = transition
     return [dqc_dt, dqp1_dt]
 
+
 model1_args = {
+
     'name': 'model1',
     'Q_p1': 1.0,
     'V_c': 1.0,
@@ -21,7 +27,9 @@ model1_args = {
     'X': 1.0,
 }
 
+
 model2_args = {
+
     'name': 'model2',
     'Q_p1': 2.0,
     'V_c': 1.0,
@@ -30,10 +38,12 @@ model2_args = {
     'X': 1.0,
 }
 
+
 t_eval = np.linspace(0, 1, 1000)
 y0 = np.array([0.0, 0.0])
 
 fig = plt.figure()
+
 for model in [model1_args, model2_args]:
     args = [
         model['Q_p1'], model['V_c'], model['V_p1'], model['CL'], model['X']
