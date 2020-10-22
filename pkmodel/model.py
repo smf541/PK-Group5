5a0cb6ba4823080b531a43251de2a92c853a2273
Lyle Hopkins <lyle.hopkins@dtc.ox.ac.uk>
2020-10-22 14:14:37 +0100

    #18 Extended model class tests

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -12,6 +12,22 @@ class Model:
         an example parameter
 
     """
-    def __init__(self, value=42):
-        self.value = value
+    def __init__(self, name, V_c: float=1.0, CL=1.0, Ka=1.0):
+        if type(name) == None:
+            raise TypeError
+        self.__Name = name
+        if type(V_c) not in [int, float]:
+            raise TypeError
+        if type(CL) not in [int, float]:
+            raise TypeError
+        if type(Ka) not in [int, float]:
+            raise TypeError
+        self.__V_c = V_c
+
+    def add_compartment(self, Q_p:float, V_p:float):
+        if type(Q_p) not in [int, float]:
+            raise TypeError
+        if type(V_p) not in [int, float]:
+            raise TypeError
+        return True
 
--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,7 +1,6 @@
 import unittest
 import pkmodel as pk
 
-
 class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
@@ -10,6 +9,86 @@ class ModelTest(unittest.TestCase):
         """
         Tests Model creation.
         """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+        with self.assertRaises(TypeError):
+            model = pk.Model()
+        model = pk.Model('nametest')
+        self.assertEqual(model.name, 'nametest')
+
+    def test_encapsulation(self):
+        """
+        Tests Model values are encapsulated.
+        """
+        model = pk.Model('nametest')
+        with self.assertRaises(AttributeError):
+            model.Name
+        with self.assertRaises(AttributeError):
+            model.V_c
+        with self.assertRaises(AttributeError):
+            model.CL
+        with self.assertRaises(AttributeError):
+            model.Ka
+        with self.assertRaises(AttributeError):
+            model._Name
+        with self.assertRaises(AttributeError):
+            model._V_c
+        with self.assertRaises(AttributeError):
+            model._CL
+        with self.assertRaises(AttributeError):
+            model._Ka
+        with self.assertRaises(AttributeError):
+            model.__Name
+        with self.assertRaises(AttributeError):
+            model.__V_c
+        with self.assertRaises(AttributeError):
+            model.__CL
+        with self.assertRaises(AttributeError):
+            model.__Ka
+
+    def test_create_defaults(self):
+        """
+        Tests Model creation default values.
+        """
+        model = pk.Model('nametest')
+        self.assertEqual(model.name, 'nametest')
+        self.assertEqual(model.v_c, 1.0)
+        self.assertEqual(model.cl, 1.0)
+        self.assertEqual(model.ka, 1.0)
+
+    def test_create_initvalues(self):
+        """
+        Tests Model creation with initial values.
+        """
+        model = pk.Model( 'nametest', V_c=1.1, CL=1.2, Ka=1.4 )
+        self.assertEqual(model.name, 'nametest')
+        self.assertEqual(model.v_c, 1.1)
+        self.assertEqual(model.cl, 1.2)
+        self.assertEqual(model.ka, 1.4)
+
+    def test_create_initvalues_typeerror(self):
+        """
+        Tests Model creation with initial values.
+        """
+        with self.assertRaises(TypeError):
+            model = pk.Model( 'name', V_c='a' )
+        with self.assertRaises(TypeError):
+            model = pk.Model( 'name', CL='b' )
+        with self.assertRaises(TypeError):
+            model = pk.Model( 'name', Ka='c' )
+
+    def test_compartments(self):
+        """
+        Tests Model compartments.
+        """
+        model = pk.Model('nametest')
+        with self.assertRaises(TypeError):
+            model.add_compartment()
+        with self.assertRaises(TypeError):
+            model.add_compartment(1.0)
+        added = model.add_compartment(1.0,1.1)
+        self.assertEqual(True, added)
+        with self.assertRaises(TypeError):
+            model.add_compartment('a','b')
+        self.assertEqual(model.list, [[1.0,1.1]])
+        model.add_compartment(1.2,1.3)
+        self.assertEqual(model.list, [[1.0,1.1],[1.2,1.3]])
 
