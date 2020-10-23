48521d534a18271ff8bc4834655dd63c8e37e32b
smf541 <stelfelnet@unitybox.de>
2020-10-23 09:27:45 +0100

    #38 modify package version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Group 5'
 author = 'Group 5'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
